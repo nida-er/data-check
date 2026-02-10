a1ea003ed2e90208a2a35bc32289da47e066b612
nidaer-byte <nidatokac@gmail.com>
2026-02-10 17:22:42 +0300

    Implement circle area calculation in demo.py

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
-
-
+import math
 def circle_area(radius):
     """Returns the area of the circle of given radius"""
-    pass  # YOUR CODE HERE
+    # Formula of circle area: A = pi * r^2
+    area = math.pi * (radius ** 2)
+    return area
